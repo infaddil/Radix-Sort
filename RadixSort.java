3136ecf838cdf7011d25dbd09bd5b15047356407
Intan Aqilah <44042512+infaddil@users.noreply.github.com>
2023-05-07 14:19:11 +0800

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -1,44 +1,44 @@
 import java.util.Arrays;
 
 public class RadixSort {
+    // declare integer array variable that the value won't change and can't be
+    // accessed outside of this class
     private final int[] array;
 
-    public RadixSort(int[] array)
-    {
+    // define RadixSort with array variable as argument
+    public RadixSort(int[] array) {
+        // update the current element as new array variable
         this.array = array;
     }
 
-    public void countingSort(int exp)
-    {
-
+    // define countingSort method
+    public void countingSort(int currentdigit) {
+        // declare the count and output variables
         int[] count = new int[10];
         int[] output = new int[array.length];
 
-        for (int value : array)
-        {
-            count[(value / exp) % 10]++;
+        //looping over each element in the integer array array
+        for (int value : array) {
+            //increment the count of the next digit
+            count[(value / currentdigit) % 10]++;
         }
-
-        for (int i = 1; i < 10; i++)
-        {
+        //
+        for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
         }
-
-        for (int i = array.length - 1; i >= 0; i--)
-        {
-            output[count[(array[i] / exp) % 10] - 1] = array[i];
-            count[(array[i] / exp) % 10]--;
+        
+        for (int i = array.length - 1; i >= 0; i--) {
+            output[count[(array[i] / currentdigit) % 10] - 1] = array[i];
+            count[(array[i] / currentdigit) % 10]--;
         }
 
         System.arraycopy(output, 0, array, 0, array.length);
     }
 
-    void sort()
-    {
+    void sort() {
         int max = Arrays.stream(array).max().getAsInt();
-        for (int exp = 1; max / exp > 0; exp *= 10)
-        {
-            countingSort(exp);
+        for (int currentdigit = 1; max / currentdigit > 0; currentdigit *= 10) {
+            countingSort(currentdigit);
         }
     }
 }
